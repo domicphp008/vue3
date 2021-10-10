03a3e2964cfa43c4a34554c4b10ce9d19bcb9367
Domicio Jose <testedomicio@hotmail.com>
2021-10-09 22:30:56 -0300

    New Loops changes in vue 3

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const app = {
     data (){
         return {
-            name: 'Domicio',
+            name: 'Domicio José',
             lastName: 'José - Curso de Vue - 3 - Redenrização condicional  no Vue3',
-            product: {
-                title: 'Filme 01',
-                description: 'Filme muito legal',
-                image: './assets/images/filme01.jpg',
-                stars: 0,
-            },
-            product2: {
-                title: 'Filme 02',
-                description: 'Filme muito legal',
-                image: './assets/images/filme02.jpg',
-                stars: 3
-            }
-
-                
+            products: [
+                {
+                    title: 'Filme 01',
+                    description: 'Filme muito legal',
+                    image: './assets/images/filme01.jpg',
+                    stars: 3,
+                },
+                {
+                    title: 'Filme 02',
+                    description: 'Filme muito legal',
+                    image: './assets/images/filme02.jpg',
+                    stars: 0
+                }
+            ]     
         }
     }
 }
